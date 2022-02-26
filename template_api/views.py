bd3950efda0b4161bb328630a15d90ae75a1609b
PJ Rape <ptrj96@gmail.com>
2022-02-26 11:06:06 -0700

    make hello world response json

--- template_api/views.py
+++ template_api/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpRequest, HttpResponse
-
+import json
 # Create your views here.
 
 
 def hello(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello World")
+    response_data = {"message": "Hello World"}
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
